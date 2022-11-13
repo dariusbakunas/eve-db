23ea3df902c8967c19afa81e9c4b4d208e681e58
Darius Bakunas-Milanowski <bakunas@gmail.com>
2022-11-13 09:26:55 -0500

    remove unused scripts

--- src/generated/client/index.js
+++ src/generated/client/index.js
@@ -1074,8 +1074,7 @@ const config = {
     "isCustomOutput": true
   },
   "relativeEnvPaths": {
-    "rootEnvPath": "../../../.env",
-    "schemaEnvPath": "../../../.env"
+    "rootEnvPath": null
   },
   "relativePath": "../../../prisma",
   "clientVersion": "4.6.1",
